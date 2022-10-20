8fecffcf225cabe6891120b4f8214dc0faa1a245
Anoop <kaur0550@algonquinlive.com>
2022-10-19 20:14:41 -0400

    add ratings

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
 
 function Card({data}){
     if(data.length === 0 )return null
@@ -11,6 +12,8 @@ function Card({data}){
           <Grid item xs={12} sm={6} md={4} key={item.id}>
             <Paper elevation={3} className="paper">
                 <img  src={item.image} alt="" className="img"/>
+                <br />
+                <Rating name="disabled" value={item.rating.rate} disabled />
             <Typography variant="body1" gutterBottom>{item.title}</Typography>
             </Paper>
           </Grid>
