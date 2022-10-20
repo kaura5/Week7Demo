0877b8f30c4ae3b8d9bf4baa2911297111fcae7d
Anoop <kaur0550@algonquinlive.com>
2022-10-19 20:00:20 -0400

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import Container from '@mui/material/Container';
+import Header from './Header';
+import Card from './Card';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com/products/`)
+    .then(res=> res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header />
+        <Card data={data}/>
+      </Container>
     </div>
   );
 }
